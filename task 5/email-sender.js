010a95b5ac156aa9f382a76a4e8c58ea46357ccb
dobbysock69 <103392445+dobbysock69@users.noreply.github.com>
2023-10-10 11:41:02 +0100

    cant use my gmail details

--- task 5/email-sender.js
+++ task 5/email-sender.js
@@ -1,17 +1,19 @@
 const nodemailer = require("nodemailer");
 
 // Create a transporter object using your SMTP configuration
+
+//Log
 const transporter = nodemailer.createTransport({
 	service: "gmail", // e.g., 'gmail'
 	auth: {
-		user: "", // your email address
-		pass: "", // your email password
+		user: "dzqdsqdqfz@gmail.com", // your email address
+		pass: "zieifj1z78prthy", // your email password
 	},
 });
 
 // Email message options
 const mailOptions = {
-	from: "shibalseki1818@gmail.com", // sender's email address
+	from: "dzqdsqdqfz@gmail.com", // sender's email address
 	to: "fahry657@gmail.com", // recipient's email address
 	subject: "Hello from Node.js", // email subject
 	text: "This is a test email sent from Node.js.", // email content (plain text)
